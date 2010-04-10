9cd22a9d5306695647c2a7c3e4f07e06a127bb62
ezedamico <ezedamico@gmail.com>
2010-04-10 22:28:38 +0000

    puta UI

---
+++ src/main.java
@@ -0,0 +1,198 @@
+
+
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import java.awt.Dimension;
+
+import java.awt.Rectangle;
+
+import javax.swing.ImageIcon;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+import java.awt.Point;
+
+public class main extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel jContentPane = null;
+	private JTextField jTextFieldFoto = null;
+	private JLabel jLabelFoto = null;
+	private JButton jButtonAbrir = null;
+	private JButton jButtonFiltro1 = null;
+	private JButton jButtonFiltro2 = null;
+	private JButton jButtonFiltro3 = null;
+	private JLabel jLabel1 = null;
+	private JButton jButtonDeshacer = null;
+	private JButton jButtonAlmacenar = null;
+	private JButton jButtonSimilar = null;
+	
+	public main() {
+		super();
+		initialize();
+	}
+
+	
+	private void initialize() {
+		this.setSize(1000, 750);
+		this.setContentPane(getJContentPane());
+		this.setTitle("Ey ey fierita ete e el proyecto toson");
+	}
+
+	
+	private JPanel getJContentPane() {
+		if (jContentPane == null) {
+			jLabel1 = new JLabel();
+			jLabel1.setText("Imagen");
+			jLabel1.setSize(new Dimension(700, 550));
+			jLabel1.setLocation(new Point(44, 104));
+			jLabelFoto = new JLabel();
+			jLabelFoto.setBounds(new Rectangle(41, 21, 228, 22));
+			jLabelFoto.setText("Ingrese la foto a procesar");
+			jContentPane = new JPanel();
+			jContentPane.setLayout(null);
+			jContentPane.add(getJTextFieldFoto(), null);
+			jContentPane.add(jLabelFoto, null);
+			jContentPane.add(getJButtonAbrir(), null);
+			jContentPane.add(getJButtonFiltro1(), null);
+			jContentPane.add(getJButtonFiltro2(), null);
+			jContentPane.add(getJButtonFiltro3(), null);
+			jContentPane.add(jLabel1, null);
+			jContentPane.add(getJButtonDeshacer(), null);
+			jContentPane.add(getJButtonAlmacenar(), null);
+			jContentPane.add(getJButtonSimilar(), null);
+		}
+		return jContentPane;
+	}
+
+	
+	private JTextField getJTextFieldFoto() {
+		if (jTextFieldFoto == null) {
+			jTextFieldFoto = new JTextField();
+			jTextFieldFoto.setBounds(new Rectangle(39, 54, 230, 28));
+		}
+		return jTextFieldFoto;
+	}
+
+	
+	private JButton getJButtonAbrir() {
+		if (jButtonAbrir == null) {
+			jButtonAbrir = new JButton();
+			jButtonAbrir.setBounds(new Rectangle(284, 57, 152, 25));
+			jButtonAbrir.setText("Abrir");
+			jButtonAbrir.addMouseListener(new java.awt.event.MouseAdapter() {
+				public void mouseClicked(java.awt.event.MouseEvent e) {
+					String foto=jTextFieldFoto.getText();
+					ImageIcon iconoImagen = new ImageIcon(foto+".jpg");
+					jLabel1.setIcon(iconoImagen);
+					
+	
+				
+
+				}
+			});
+		}
+		return jButtonAbrir;
+	}
+
+	
+	private JButton getJButtonFiltro1() {
+		if (jButtonFiltro1 == null) {
+			jButtonFiltro1 = new JButton();
+			jButtonFiltro1.setText("Filtro 1");
+			jButtonFiltro1.setLocation(new Point(789, 165));
+			jButtonFiltro1.setSize(new Dimension(120, 26));
+			jButtonFiltro1.addMouseListener(new java.awt.event.MouseAdapter() {
+				public void mouseClicked(java.awt.event.MouseEvent e) {
+					System.out.println("mouseClicked()"); 
+				}
+			});
+		}
+		return jButtonFiltro1;
+	}
+
+	
+	private JButton getJButtonFiltro2() {
+		if (jButtonFiltro2 == null) {
+			jButtonFiltro2 = new JButton();
+			jButtonFiltro2.setText("Filtro 2");
+			jButtonFiltro2.setSize(new Dimension(120, 26));
+			jButtonFiltro2.setLocation(new Point(789, 301));
+			jButtonFiltro2.addMouseListener(new java.awt.event.MouseAdapter() {
+				public void mouseClicked(java.awt.event.MouseEvent e) {
+					System.out.println("mouseClicked()"); 
+				}
+			});
+		}
+		return jButtonFiltro2;
+	}
+
+	private JButton getJButtonFiltro3() {
+		if (jButtonFiltro3 == null) {
+			jButtonFiltro3 = new JButton();
+			jButtonFiltro3.setText("Filtro 3");
+			jButtonFiltro3.setSize(new Dimension(120, 26));
+			jButtonFiltro3.setLocation(new Point(789, 441));
+			jButtonFiltro3.addMouseListener(new java.awt.event.MouseAdapter() {
+				public void mouseClicked(java.awt.event.MouseEvent e) {
+					System.out.println("mouseClicked()"); 
+				}
+			});
+		}
+		return jButtonFiltro3;
+	}
+
+
+	
+	private JButton getJButtonDeshacer() {
+		if (jButtonDeshacer == null) {
+			jButtonDeshacer = new JButton();
+			jButtonDeshacer.setText("Deshacer");
+			jButtonDeshacer.setSize(new Dimension(152, 25));
+			jButtonDeshacer.setLocation(new Point(585, 57));
+			jButtonDeshacer.addMouseListener(new java.awt.event.MouseAdapter() {
+				public void mouseClicked(java.awt.event.MouseEvent e) {
+					System.out.println("mouseClicked()"); 
+				}
+			});
+		}
+		return jButtonDeshacer;
+	}
+
+
+	
+	private JButton getJButtonAlmacenar() {
+		if (jButtonAlmacenar == null) {
+			jButtonAlmacenar = new JButton();
+			jButtonAlmacenar.setText("Almacenar");
+			jButtonAlmacenar.setLocation(new Point(97, 677));
+			jButtonAlmacenar.setSize(new Dimension(184, 28));
+			jButtonAlmacenar.addMouseListener(new java.awt.event.MouseAdapter() {
+				public void mouseClicked(java.awt.event.MouseEvent e) {
+					System.out.println("mouseClicked()"); 
+				}
+			});
+		}
+		return jButtonAlmacenar;
+	}
+
+
+	private JButton getJButtonSimilar() {
+		if (jButtonSimilar == null) {
+			jButtonSimilar = new JButton();
+			jButtonSimilar.setText("Similar");
+			jButtonSimilar.setSize(new Dimension(184, 28));
+			jButtonSimilar.setLocation(new Point(397, 677));
+			jButtonSimilar.addMouseListener(new java.awt.event.MouseAdapter() {
+				public void mouseClicked(java.awt.event.MouseEvent e) {
+					System.out.println("mouseClicked()"); 
+				}
+			});
+		}
+		return jButtonSimilar;
+	}
+	
+	
+
+}  
