f4e1d0512ea3ed2ab2a727c857fb41746745a3a2
ezedamico@gmail.com <ezedamico@gmail.com@bec64cff-d88b-433d-6db2-07560b718459>
2010-04-11 22:06:46 +0000

    sistemita de filtros

--- trunk/src/model/ColorApp.java
+++ trunk/src/model/ColorApp.java
@@ -1,27 +1,6 @@
 package model;
-/*
- * Apollo - Motion capture and animation system
- * Copyright (c) 2005 Apollo
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- * http://www.gnu.org/copyleft/gpl.html
- *
- * @author Giovane.Kuhn - brain@netuno.com.br
- *
- */
+
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
