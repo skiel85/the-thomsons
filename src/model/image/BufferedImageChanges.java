71d15cc43e26ed0e8df7656105410b577ebeec75
nicohen <nicohen@bec64cff-d88b-433d-6db2-07560b718459>
2010-05-09 21:28:35 +0000

    deshacer corregido

---
+++ src/model/image/BufferedImageChanges.java
@@ -0,0 +1,38 @@
+package model.image;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BufferedImageChanges {
+
+	private static List<BufferedImage> changes;
+    private static BufferedImageChanges instance = null;
+    
+    private BufferedImageChanges() {}
+ 
+    private static void createInstance() {
+        if (instance == null) { 
+            instance = new BufferedImageChanges();
+            changes = new ArrayList<BufferedImage>();
+        }
+    }
+ 
+    public static BufferedImageChanges getInstance() {
+        if (instance == null) createInstance();
+        return instance;
+    }
+    
+    public void changeImage(BufferedImage bi) {
+    	changes.add(bi);
+    }
+    
+    public BufferedImage undo() {
+    	if(changes.size()>1) {
+    		BufferedImage bi = changes.get(changes.size()-1);
+	    	changes.remove(changes.size()-1);
+	    	return bi;
+    	}
+    	return changes.get(0);
+    }
+}
